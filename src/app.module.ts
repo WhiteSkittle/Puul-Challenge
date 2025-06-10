57d8a0f3dd09316084a9300035b5b52bbc5cd389
Ezequiel Bello <ebello@blueskittle.com>
2025-06-10 14:15:18 -0600

    feat: configure global pipes and filters in main application bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './modules/users/users.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { AnalyticsModule } from './modules/analytics/analytics.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, TasksModule, AnalyticsModule],
 })
 export class AppModule {}
